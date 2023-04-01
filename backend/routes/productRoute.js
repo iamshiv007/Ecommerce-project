2d821bf3b39d7da5a3566d7ac5ec88e79e314e49
iamshiv007 <iamshiv20032003@gmail.com>
2023-04-01 18:02:55 +0530

    feat:User api completed

--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -1,9 +1,10 @@
 const express = require('express')
-const { createProduct, getAllProducts, updateProduct, deleteProduct, getProductDetails } = require('../controllers/productController')
+const { createProduct, getAdminProducts, getAllProducts, updateProduct, deleteProduct, getProductDetails } = require('../controllers/productController')
+const { isAuthenticateUser, authorizeRoles } = require('../middleware/auth')
 const router = express.Router()
 
-router.route('/products').get(getAllProducts)
-router.route('/product/new').post(createProduct)
+router.route('/products').get(isAuthenticateUser, authorizeRoles("Admin"), getAllProducts)
+router.route('/admin/product/new').post(isAuthenticateUser, authorizeRoles("Admin"), createProduct)
 router.route('/product/:id').put(updateProduct).delete(deleteProduct).get(getProductDetails)
 
 module.exports = router
