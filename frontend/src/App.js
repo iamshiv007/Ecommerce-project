fb9405f5d671ff5fb66178a6b895dea753325d45
iamshiv007 <iamshiv20032003@gmail.com>
2023-04-05 14:05:21 +0530

    feat: loginSignup and profile component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,27 @@
+import React from 'react'
 import './App.css';
+import { BrowserRouter as Router, Route, Switch} from 'react-router-dom'
 import { Footer } from './component/layout/Footer/Footer';
 import { Header } from './component/layout/Header/Header';
-import { BrowserRouter as Router, Route, Switch} from 'react-router-dom'
+import { Loader } from './component/layout/Loader/Loader';
+import { LogInSignUp } from './component/User/LogInSignUp';
+import { ForgotPassword } from './component/User/ForgotPassword';
+import { ResetPassword } from './component/User/ResetPassword';
+import { UpdatePassword } from './component/User/UpdatePassword';
+import { Profile } from './component/User/Profile';
+import { UpdateProfile } from './component/User/UpdateProfile';
+import { About } from './component/layout/About/About';
 
 function App() {
   return (
     <Router>
+      <Header/>
       <Switch>
-     <div><Header/></div>
-     <div><Footer/></div>
+     <Route exact path='/' component={LogInSignUp} />
+     <Route exact path='/profile' component={Profile} />
      </Switch>
     </Router>
-  );
+  )
 }
 
-export default App;
+export default App
